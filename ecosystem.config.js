5cf63f4b5bd4b8595986bec534deb23d1efb7a71
yranjan <yranjan@in.imshealth.com>
2018-12-08 09:01:46 +0530

    added ecosytem :hammer:

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "notifier",
+      script: "./server/bin/www",
+      error_file: __dirname + "/../logs/notifier/err.log",
+      out_file: __dirname + "/../logs/notifier/out.log",
+      watch: true,
+      merge_logs: true,
+      exec_mode: "cluster",
+      instances: 2,
+      log_date_format: "YYYY-MM-DD HH:mm:ss.SSS",
+      env: {
+        PORT: 3000,
+        NODE_ENV: "development"
+      },
+      env_production: {
+        PORT: 80,
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
